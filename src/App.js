67c72280b4927fed5833697bb09fe74aa53a579d
mariusparasca <mrs.parasca@gmail.com>
2020-04-26 10:22:33 +0300

    test if selector works

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import RequestComponent from 'components/RequestComponent/RequestComponent';
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 
-// import { useFirestoreConnect } from 'react-redux-firebase';
-// import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded } from 'react-redux-firebase';
+import { useSelector } from 'react-redux';
 
 const App = () => {
-  // useFirestoreConnect([
-  //   { collection: 'locations' }, // or 'todos'
-  // ]);
-  // const todos = useSelector(state => state.firestore.ordered.locations);
+  useFirestoreConnect([
+    { collection: 'users' }, // or 'todos'
+  ]);
+  const todos = useSelector(state => state.firestore.ordered.users);
 
-  // console.log('todos', todos);
+  console.log('isLoaded(todos', isLoaded(todos));
+  console.log('todos', todos);
 
   return (
     <ErrorBoundary>
---
+++ src/utils/fbConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyCAJTKRmN6midO13QfdSHMzdHbNnmJtuhc',
+  authDomain: 'av-weather-53882.firebaseapp.com',
+  databaseURL: 'https://av-weather-53882.firebaseio.com',
+  projectId: 'av-weather-53882',
+  storageBucket: 'av-weather-53882.appspot.com',
+  messagingSenderId: '896818180379',
+  appId: '1:896818180379:web:714f2d73ba5a6b06e8e3be',
+  measurementId: 'G-RGT36Y5FVJ',
+};
